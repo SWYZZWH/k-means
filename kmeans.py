f1f36fbe58a672811b44ed7b3f67a664cbb4e406
zwh <zhaoweihao.zwh@alibaba-inc.com>
2020-12-15 22:24:39 +0800

    修复了格式问题，重写了test.py

--- kmeans.py
+++ kmeans.py
@@ -1,4 +1,5 @@
 # -*- coding:UTF-8 -
+from abc import get_cache_token
 from utility import *
 import numpy as np
 import logging
@@ -20,17 +21,35 @@ class kmeans_basic:
         self.weighted = weighted
         self.weights = weights
         self.iter = iter
+        self.runtime_iter = 0
+        self.init_cost = self.get_cost()
 
+    def get_init_cost(self):
+        return self.init_cost
+
+    def get_final_cost(self):
+        return self.final_cost
+        
+    def get_runtime_iter(self):
+        return self.runtime_iter + 1
+    
+    def get_init_iter(self):
+        return 0
+    
+    def get_init_time(self):
+        return 0
+    
+    def get_total_time(self):
+        return self.total_time
+    
     def get_cost(self):
         distances = dis(self.points, self.centers)
         return cost(distances)
-    
-    def get_time(self):
-        return self.time
 
     def train(self):
         start = time.time_ns() 
         for i in range(self.iter):
+            self.runtime_iter += 1
             distances = dis(self.points, self.centers)
             cst = cost(distances)
             self.time = time.time_ns() - start
@@ -45,12 +64,15 @@ class kmeans_basic:
                 else:
                     new_centers[j] = self.centers[j]
             if np.array_equal(new_centers, self.centers):
+                self.total_time = time.time_ns() - start
+                self.final_cost = self.get_cost()
                 if self.weighted:
                     print("weighted k-means converges at {} iterations".format(i))
                 else:
                     print("basic k-means converges at {} iterations".format(i))                    
                 return self.centers
             self.centers = new_centers
+        
 
         if self.weighted:
             print("weighted k-means hasn't converged at {} iterations".format(self.iter))
@@ -75,12 +97,29 @@ class kmeans_plusplus:
         # 初始分布
         distances = dis(self.points, self.centers)
         self.distribution = np.min(distances, axis=1) / np.sum(np.min(distances, axis=1)) 
+        self.runtime_iter = 0
+
+    def get_init_cost(self):
+        return self.init_cost
+
+    def get_final_cost(self):
+        return self.final_cost
+        
+    def get_runtime_iter(self):
+        return self.runtime_iter
     
-    def get_time(self):
-        return self.time
+    def get_init_iter(self):
+        return self.k
+    
+    def get_init_time(self):
+        return self.init_time
+    
+    def get_total_time(self):
+        return self.total_time
     
     def get_cost(self):
-        return self.cost
+        distances = dis(self.points, self.centers)
+        return cost(distances)
 
     def train(self):        
         # k-means++ 初始化，并不保证收敛，因此还要跑 k-means
@@ -93,14 +132,17 @@ class kmeans_plusplus:
             distances = dis(self.points, self.centers)
             self.distribution = np.min(distances, axis=1) / np.sum(np.min(distances, axis=1)) 
             self.cost = cost(distances)
-        self.time = time.time_ns() - start
+
+        self.init_cost = self.get_cost()
+        self.init_time = time.time_ns() - start
     
-        print("cost after init:{}, init time:{}".format(self.cost, self.time))
+        #print("cost after init:{}, init time:{}".format(self.cost, self.time))
         kmeans = kmeans_basic(self.points, self.k, self.iter, centers=self.centers)
         kmeans.train()
-        self.time = kmeans.get_time()
-        self.cost = kmeans.get_cost()
-            
+        self.total_time = kmeans.get_total_time() + self.init_time
+        self.final_cost = kmeans.get_cost()
+        self.runtime_iter = kmeans.get_runtime_iter()
+        
 
 class kmeans_parallel:
         """
@@ -109,9 +151,10 @@ class kmeans_parallel:
         最后可以直接根据 weights 直接筛选出 k 个中心； 也可以随机选 k 个中心， 把 k * iter 作为 points 运行 k-means 算法
         但要注意这里的 k-means 算法算距离的时候要乘上 weights
         """
-        def __init__(self, points, k, iter, weighted = False):
+        def __init__(self, points, k, l, iter, weighted = False):
             self.n, self.dim = points.shape
             self.k = k
+            self.l = l
             self.iter = iter
             self.weighted = weighted
             self.points = points
@@ -119,44 +162,68 @@ class kmeans_parallel:
             # 初始分布
             distances = dis(self.points, self.centers)
             self.distribution = np.min(distances, axis=1) / np.sum(np.min(distances, axis=1)) 
-            self.time = 0
+            self.init_iter = 0
+            self.runtime_iter = 0
+        
+        def get_init_cost(self):
+            return self.init_cost
+
+        def get_final_cost(self):
+            return self.final_cost
+            
+        def get_runtime_iter(self):
+            return self.runtime_iter
+        
+        def get_init_iter(self):
+            return self.init_iter
+        
+        def get_init_time(self):
+            return self.init_time
+        
+        def get_total_time(self):
+            return self.total_time
 
         def get_cost(self):
             distances = dis(self.points, self.centers)
             return cost(distances)
             
-        def get_time(self):
-            return self.time
-
         def get_weights(self):
             distances = dis(self.points, self.centers)
             counts = np.zeros(distances.shape)
-            counts[range(counts.shape[0]), np.argmin(distances, axis=1)] = 1
-            return np.sum(counts, axis=0)/ np.sum(np.sum(counts, axis=0))
+            counts[range(distances.shape[0]), np.argmin(distances, axis=1)] = 1
+            count = np.array([np.count_nonzero(counts[:, i]) for i in range(distances.shape[1])])
+            return count/np.sum(count)
+            #return np.sum(counts, axis=0)/ np.sum(np.sum(counts, axis=0))
 
         def train(self):
             """
-            centers num : self.iter * self.k 
+            centers num : self.iter * self.l
             """
             start = time.time_ns()
-            for i in range(self.iter - 1):
-                next_centers = self.points[np.random.choice(len(self.points), self.k, p=self.distribution), :]
+            for i in range(self.iter):
+                self.init_iter += 1
+                next_centers = self.points[np.random.choice(len(self.points), self.l, p=self.distribution), :]
                 self.centers = np.vstack((self.centers, next_centers))
                 # 更新分布，已经被选中的点是不会被再次选中的
                 distances = dis(self.points, self.centers)
                 self.distribution = np.min(distances, axis=1) / np.sum(np.min(distances, axis=1)) 
-            self.time = time.time_ns() - start
-            print("cost after init:{}, init time:{}".format(self.get_cost(), self.time))
+
+            self.init_cost = self.get_cost()
+            self.init_time = time.time_ns() - start
+            #rint("cost after init:{}, init time:{}".format(self.get_cost(), self.time))
             
             weights = self.get_weights()
-            if not self.weighted:
-                self.centers = self.centers[np.random.choice(len(self.centers), self.k, p=weights), :]
-            else:
-                # iter 可以取很大不用担心性能问题，因为可以提前收敛
-                kmeans_weighted = kmeans_basic( points=self.centers, k=self.k, iter=self.iter * 1000, weighted = True, weights=weights)
-                kmeans_weighted.train()
-                self.time = kmeans_weighted.get_time()
-                self.centers =  kmeans_weighted.centers
+            # if not self.weighted:
+            #     self.centers = self.centers[np.random.choice(len(self.centers), self.k, p=weights), :]
+            # else:
+            # iter 可以取很大不用担心性能问题，因为可以提前收敛
+            kmeans_weighted = kmeans_basic( points=self.centers, k=self.k, iter=self.iter * 1000, weighted = True, weights=weights)
+            kmeans_weighted.train()
+            self.centers =  kmeans_weighted.centers
+            self.total_time = kmeans_weighted.get_total_time() + self.init_time
+            self.runtime_iter = kmeans_weighted.get_runtime_iter() 
+            self.final_cost = self.get_cost()
+            
 
             
 
--- test.py
+++ test.py
@@ -1,43 +1,96 @@
+
 from kmeans import *
 from data import *
 
 def benchmark(title, points, k, iter, dup):
-    basic_cost_avg, pp_cost_avg, par_cost_avg = 0,0,0
-    basic_time_avg, pp_time_avg, par_time_avg = 0,0,0
+    name_lst = ["basic", "pp", "par", "par2"]
+    model_lst = [
+        kmeans_basic(points, k, iter=iter), 
+        kmeans_plusplus(points, k, iter), 
+        kmeans_parallel(points, k,  k // 2 , 5, weighted=True ),
+        kmeans_parallel(points, k, k * 2, 5, weighted=True)
+    ]
+    init_cost_lst = [0,0,0,0]
+    final_cost_lst = [0,0,0,0]
+    init_iter_lst = [0,0,0,0]
+    iter_lst = [0,0,0,0]
+    init_time_lst = [0,0,0,0]
+    total_time_lst = [0,0,0,0]
 
     for i in range(dup):
-        basic = kmeans_basic(points, k, iter=iter)
-        pp = kmeans_plusplus(points, k, iter)
-        par_weighted = kmeans_parallel(points, k, iter//100, weighted=True)
-        basic.train()
-        pp.train()
-        par_weighted.train()
+        for j, model in enumerate(model_lst):
+            model.train()
+            init_cost_lst[j] += model.get_init_cost() / dup
+            final_cost_lst[j] += model.get_final_cost() / dup
+            init_iter_lst[j] += round(model.get_init_iter()) / dup
+            iter_lst[j] += round(model.get_init_iter()) / dup
+            init_time_lst[j] += model.get_init_time()/ dup
+            total_time_lst[j] += model.get_total_time()/ dup
 
-        print("final cost\n random_init:{}, kmeans++_init:{}, kmeans||_init:{} \
-            time\n random_init:{}, kmeans++_init:{}, kmeans||_init:{}  \
-        ".format(    
-            basic.get_cost(),
-            pp.get_cost(),
-            par_weighted.get_cost(),
-            basic.get_time(),
-            pp.get_time(),
-            par_weighted.get_time()
-        ))
-        basic_cost_avg += basic.get_cost() / dup
-        pp_cost_avg += pp.get_cost() / dup
-        par_cost_avg += par_weighted.get_cost() / dup
-        basic_time_avg += basic.get_time() / dup
-        pp_time_avg += pp.get_time() / dup
-        par_time_avg += par_weighted.get_time() / dup
 
-    print("{}\n on average\n final cost\n random_init:{}, kmeans++_init:{}, kmeans||_init:{} \
-            time\n random_init:{}, kmeans++_init:{}, kmeans||_init:{}  \
-        ".format(    
-            title, basic_cost_avg, pp_cost_avg, par_cost_avg,basic_time_avg, pp_time_avg, par_time_avg
-    ))
     with open("data/result.data", "a") as f:
         f.write(title + "\n")
-        f.write("{},{},{},{},{},{}\n".format(basic_cost_avg, pp_cost_avg, par_cost_avg,basic_time_avg, pp_time_avg, par_time_avg))
+        for j in range(len(name_lst)):
+            f.write("\tmodel:{}\n".format(name_lst[j]))
+            f.write("\t cost_init:{}, cost_final:{}, init_iter:{}, runtime_iter:{}, init_time:{}, total_time:{}\n".format(
+                init_cost_lst[j], final_cost_lst[j], init_iter_lst[j], iter_lst[j], init_time_lst[j], total_time_lst[j]
+            ))
+
+    
+
+# def benchmark(title, points, k, iter, dup):
+#     basic_iter_avg, pp_iter_avg, par_iter_avg, par2_iter_avg = 0,0,0,0
+#     pp_init_iter_avg, par_init_iter_avg, par2_init_iter_avg = 0,0,0,0
+#     basic_cost_avg, pp_cost_avg, par_cost_avg, par2_cost_avg  = 0,0,0,0
+#     basic_time_avg, pp_time_avg, par_time_avg, par2_time_avg = 0,0,0,0
+
+#     for i in range(dup):
+#         basic = kmeans_basic(points, k, iter=iter)
+#         pp = kmeans_plusplus(points, k, iter)
+#         par_weighted = kmeans_parallel(points, k,  k // 2 , 5, weighted=True )
+#         par_weighted_2 = 
+#         basic.train()
+#         pp.train()
+#         par_weighted.train()
+#         par_weighted_2.train()
+
+#         print("final cost\n random_init:{}, kmeans++_init:{}, kmeans||_init:{}, kmeans||2_init:{}\
+#             time\n random_init:{}, kmeans++_init:{}, kmeans||_init:{}, kmeans||2_init:{} \
+#         ".format(    
+#             basic.get_cost(),
+#             pp.get_cost(),
+#             par_weighted.get_cost(),
+#             par_weighted_2.get_cost(),
+#             basic.get_time(),
+#             pp.get_time(),
+#             par_weighted.get_time(),
+#             par_weighted_2.get_time()
+#         ))
+#         basic_iter_avg  += basic.get_runtime_iter() / dup
+#         pp_iter_avg  += pp.get_runtime_iter() / dup
+#         par_iter_avg  += par_weighted.get_runtime_iter() / dup
+#         par2_iter_avg  += par_weighted_2.get_runtime_iter() / dup
+#         pp_iter_avg  += pp.get_runtime_iter() / dup
+#         par_iter_avg  += par_weighted.get_runtime_iter() / dup
+#         par2_iter_avg  += par_weighted_2.get_runtime_iter() / dup
+#         basic_cost_avg += basic.get_cost() / dup
+#         pp_cost_avg += pp.get_cost() / dup
+#         par_cost_avg += par_weighted.get_cost() / dup
+#         par2_cost_avg += par_weighted_2.get_cost() / dup
+#         basic_time_avg += basic.get_time() / dup
+#         pp_time_avg += pp.get_time() / dup
+#         par_time_avg += par_weighted.get_time() / dup
+#         par2_time_avg += par_weighted_2.get_time() / dup
+
+#     print("{}\n on average\n final cost\n random_init:{}, kmeans++_init:{}, kmeans||_init:{}, kmeans||2_init:{} \
+#             time\n random_init:{}, kmeans++_init:{}, kmeans||_init:{}, kmeans||2_init:{}  \
+#         ".format(    
+#             title, basic_cost_avg, pp_cost_avg, par_cost_avg, par2_cost_avg, basic_time_avg, pp_time_avg, par_time_avg, par2_time_avg
+#     ))
+#     with open("data/result.data", "a") as f:
+#         f.write(title + "\n")
+#         f.write("{},{},{},{},{},{},{},{}\n".format(basic_cost_avg, pp_cost_avg,
+#          par_cost_avg, par2_cost_avg, basic_time_avg, pp_time_avg, par_time_avg, par2_time_avg))
 
 if __name__ == "__main__":
     iter = 1000
@@ -52,8 +105,4 @@ if __name__ == "__main__":
     for R in [1, 10, 100]:
         for k in [20, 50, 100]: 
             guass = load_data_guass(R, k)
-            benchmark("guass_{}_{}".format(R,k), guass, k, iter, 11)
-    
-    
-
-    
+            benchmark("guass_{}_{}".format(R,k), guass, k, iter, 10)
